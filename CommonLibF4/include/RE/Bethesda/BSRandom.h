c3a31958e0c2cbe074e21c418495c1e7de6ac9c7
Flenarn <smallmustard@gmail.com>
2024-06-11 18:49:49 +0200

    feat: Inline function as it should be.

--- CommonLibF4/include/RE/Bethesda/BSRandom.h
+++ CommonLibF4/include/RE/Bethesda/BSRandom.h
@@ -4,7 +4,7 @@ namespace RE
 {
 	namespace BSRandom
 	{
-		std::uint32_t UnsignedInt(unsigned int aMin, unsigned int aMax)
+		inline std::uint32_t UnsignedInt(unsigned int aMin, unsigned int aMax)
 		{
 			using func_t = decltype(&BSRandom::UnsignedInt);
 			static REL::Relocation<func_t> func{ REL::ID(2267950) };
