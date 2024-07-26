4562ed5b84867177ed4e2f76d766c3105c5d698f
flenarn <smallmustard@gmail.com>
2024-07-26 13:10:44 +0200

    feat: `TravelMarkerStateChange::Event` declared.  # Please enter the commit message for your changes. Lines starting

--- CommonLibF4/include/RE/Bethesda/Events.h
+++ CommonLibF4/include/RE/Bethesda/Events.h
@@ -158,6 +158,18 @@ namespace RE
 		static_assert(sizeof(Event) == 0x8);
 	}
 
+	namespace TravelMarkerStateChange
+	{
+		struct Event
+		{
+			BSPointerHandle<TESObjectREFR, BSUntypedPointerHandle<21, 5>> markerRef;	// 00
+			MapMarkerData* data;														// 08
+			std::uint32_t selectedMarkerIndex;											// 10					
+			bool discovered;															// 14
+		};
+		static_assert(sizeof(Event) == 0x18);
+	}
+
 	struct BGSActorEvent
 	{
 	public:
