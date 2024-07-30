731fcc742b8376c62b37d6c87db08dabb9616ce2
flenarn <smallmustard@gmail.com>
2024-07-30 15:43:47 +0200

    fix: Missing import.

--- CommonLibF4/include/RE/Bethesda/PipboyDataManager.h
+++ CommonLibF4/include/RE/Bethesda/PipboyDataManager.h
@@ -12,6 +12,7 @@
 #include "RE/Bethesda/PipboyRadioData.h"
 #include "RE/Bethesda/PipboySpecialData.h"
 #include "RE/Bethesda/PipboyStatsData.h"
+#include "RE/Bethesda/PipboyStatusData.h"
 #include "RE/Bethesda/PipboyThrottleManager.h"
 #include "RE/Bethesda/PipboyValue.h"
 #include "RE/Bethesda/PipboyWorkshopData.h"
