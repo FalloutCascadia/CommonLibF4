4cb724858beb925c0af11b4b8c2b5f3712a646b9
FlenarnTemp <FlenarnTemp@users.noreply.github.com>
2025-06-10 08:39:51 +0000

    maintenance

--- include/RE/S/SubtitleInfo.h
+++ include/RE/S/SubtitleInfo.h
@@ -7,14 +7,12 @@ namespace RE
 	class SubtitleInfo
 	{
 	public:
-		
-
 		// members
-		BSPointerHandle<TESObjectREFR, BSUntypedPointerHandle<21, 5>> speaker;	// 00
-		BSFixedStringCS subtitleText;											// 08
-		TESTopicInfo* topicInfo;												// 10
-		SUBTITLE_PRIORITY priority;												// 18
-		float distFromPlayer;													// 1C
+		BSPointerHandle<TESObjectREFR, BSUntypedPointerHandle<21, 5>> speaker;         // 00
+		BSFixedStringCS                                               subtitleText;    // 08
+		TESTopicInfo*                                                 topicInfo;       // 10
+		SUBTITLE_PRIORITY                                             priority;        // 18
+		float                                                         distFromPlayer;  // 1C
 	};
 	static_assert(sizeof(SubtitleInfo) == 0x20);
 }
