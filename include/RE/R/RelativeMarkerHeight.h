108b2edb305c5eea5c24fb7ed49f3cac962dec6d
FlenarnDev <FlenarnDev@users.noreply.github.com>
2025-08-08 13:03:29 +0000

    maintenance

--- include/RE/R/RelativeMarkerHeight.h
+++ include/RE/R/RelativeMarkerHeight.h
@@ -2,7 +2,8 @@
 
 namespace RE
 {
-	enum class RelativeMarkerHeight : std::int32_t {
+	enum class RelativeMarkerHeight : std::int32_t
+	{
 		kInvalid = 0x0,
 		kBelowPlayer = 0x1,
 		kLevelWithPlayer = 0x2,
