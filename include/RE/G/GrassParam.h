ad537a192c9a504964a2c04a95950da2865eb771
FlenarnDev <FlenarnDev@users.noreply.github.com>
2025-11-08 12:05:41 +0000

    maintenance

--- include/RE/G/GrassParam.h
+++ include/RE/G/GrassParam.h
@@ -6,17 +6,17 @@ namespace RE
 	{
 	public:
 		// members
-		const char* geomFilename;	// 00
-		std::uint32_t grassID;	// 08
-		float         positionRange;	// 0C
-		float         heightRange;	// 10
-		float         colorRange;	// 14
-		float         waveOffsetRange;	// 18
-		float         wavePeriod;	// 1C
-		bool          vertexLighting;	// 20
-		bool          uniformScaling;	// 21
-		bool          fitToSlope;	// 22
-		bool          wind;	// 23
+		const char*   geomFilename;     // 00
+		std::uint32_t grassID;          // 08
+		float         positionRange;    // 0C
+		float         heightRange;      // 10
+		float         colorRange;       // 14
+		float         waveOffsetRange;  // 18
+		float         wavePeriod;       // 1C
+		bool          vertexLighting;   // 20
+		bool          uniformScaling;   // 21
+		bool          fitToSlope;       // 22
+		bool          wind;             // 23
 	};
 	static_assert(sizeof(GrassParam) == 0x28);
 }
--- include/RE/T/TESGrassAreaParam.h
+++ include/RE/T/TESGrassAreaParam.h
@@ -5,8 +5,8 @@ namespace RE
 	class TESGrassAreaParam
 	{
 	public:
-		GrassParam grassParam;	// 00
-		float      density[9];	// 28
+		GrassParam grassParam;  // 00
+		float      density[9];  // 28
 	};
 	static_assert(sizeof(TESGrassAreaParam) == 0x50);
 }
