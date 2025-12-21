740d0a4662a20d954a3a12922524a347cedf001a
FlenarnDev <FlenarnDev@users.noreply.github.com>
2025-12-21 09:34:12 +0000

    maintenance

--- include/RE/B/BSGraphics.h
+++ include/RE/B/BSGraphics.h
@@ -694,8 +694,8 @@ namespace RE
 		{
 		public:
 			// members
-			VertexBuffer* vb;	// 00
-			std::uint32_t dataStride;	// 08
+			VertexBuffer* vb;          // 00
+			std::uint32_t dataStride;  // 08
 		};
 		static_assert(sizeof(DynamicTriShapeData) == 0x10);
 
